9fd91df2f6722349e29fc62ca5f45a6fa5e3d1b2
forbearing <fredjoseph863@gmail.com>
2018-06-20 15:01:50 +0800

    Gentoo-build

---
+++ Gentoo-build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+##############################################################
+#Funcation:				Shell scripts for auto install Gentoo
+#Usage:					bash Gentoo_build.sh
+#Author:				hybfkuf
+#Date					2018-06-20
+##############################################################
